b84293ee8ec0f54044b7c5405b5e3232cb7d4c49
omaralba <cesarlionelflores@gmail.com>
2025-05-22 19:08:53 -0600

    feat: Add delete_task method

--- lib/todo.rb
+++ lib/todo.rb
@@ -8,7 +8,19 @@ module Todo
   end
 
   def find_task(uuid)
-    task_file = JSON.parse File.read('tasks.json')
-    task_file.select { |task| task['id'] == uuid }.first #|| "Task #{uuid} not found"
+    Todo.list_tasks.find { |task| task['id'] == uuid }
+
+    # task_file = JSON.parse File.read('tasks.json')
+    # task_file.select { |task| task['id'] == uuid }.first # || "Task #{uuid} not found"
+  end
+
+  def delete_task(uuid)
+    task_to_delete = Todo.find_task uuid
+
+    return if task_to_delete.nil?
+
+    tasks_updated = Todo.list_tasks.delete_if { |task| task['id'] == uuid }
+
+    task_to_delete
   end
 end
